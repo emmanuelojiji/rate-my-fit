eafdfee14a536112c7cf715a900067d6515eed41
emmanuelojiji <emmanuelojiji@hotmail.com>
2022-09-14 10:50:24 +0100

    Login test

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -4,7 +4,7 @@ import { auth } from "../FirebaseConfig";
 const Profile = () => {
   return (
     <div className="profile">
-      <h1>Profile</h1>
+      
       <h1>Hello {auth.currentUser.displayName}</h1>
     </div>
   );
